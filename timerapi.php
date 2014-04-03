046df1bf28212256fbc027150cc246475420c4c7
cbdev <cb@cbcdn.com>
2014-04-04 01:17:35 +0200

    cors fix

--- timerapi.php
+++ timerapi.php
@@ -3,23 +3,28 @@
 	TimerAPI PHP backend. Queries the SQLite Database and pushes all
 	results via JSON.
 
-	  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-						Version 2, December 2004 
+	This program is free software. It comes without any warranty, to
+	the extent permitted by applicable law. You can redistribute it
+	and/or modify it under the terms of the Do What The Fuck You Want
+	To Public License, Version 2, as published by Sam Hocevar and 
+	reproduced below.
 
-	 Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+	Version 2, December 2004 
 
-	 Everyone is permitted to copy and distribute verbatim or modified 
-	 copies of this license document, and changing it is allowed as long 
-	 as the name is changed. 
+	Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
 
-				DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-	   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+		Everyone is permitted to copy and distribute verbatim or modified 
+		copies of this license document, and changing it is allowed as long 
+		as the name is changed. 
 
-	  0. You just DO WHAT THE FUCK YOU WANT TO.
+	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+	TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
 
+		0. You just DO WHAT THE FUCK YOU WANT TO.
 	*/
 
-	$db=new PDO("sqlite:/var/www/jsontimers/timers.db3");
+	$db=new PDO("sqlite:/var/www/databases/timers.db3");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 	
 	$retVal["status"]="ok";
@@ -34,5 +39,6 @@
 	}
 	
 	header("Content-type: application/json");
+	header("Access-Control-Allow-Origin: *");
 	print(json_encode($retVal));
 ?>
